199ca01513982797b997f0e6f426e3f6b03152c5
BuildTools <unconfigured@null.spigotmc.org>
2022-10-24 09:16:08 +0300

    Fix deletion and adding autotest

--- src/main/java/net/uart/uart_marketplace/repositpories/MarketItemRepository.java
+++ src/main/java/net/uart/uart_marketplace/repositpories/MarketItemRepository.java
@@ -14,6 +14,7 @@ public interface MarketItemRepository extends PagingAndSortingRepository<MarketI
 
     boolean existsByID(String ID);
 
+    @Transactional
     void deleteByID(String ID);
 
     @Modifying
